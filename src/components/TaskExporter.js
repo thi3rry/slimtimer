7595656208d7b674693c881acf561be88ff10653
Thierry Poinot <thierry@shoot-shoot.com>
2021-01-06 09:59:23 +0100

    fix exporter

--- src/components/TaskExporter.js
+++ src/components/TaskExporter.js
@@ -3,31 +3,42 @@ import {useState} from "preact/hooks";
 
 export default function TaskExporter({tasks, updateTasks}) {
 
-    const [currentExportedTasks, setCurrentExportedTasks] = useState(JSON.stringify(tasks));
+    const [currentExportedTasks, setCurrentExportedTasks] = useState(JSON.stringify([...tasks]));
     const [exportModalVisible, setExportModalVisible] = useState(false);
     const [importMerge, setImportMerge] = useState(true);
-
     return (
         <>
-            <button onClick={() => {
-                setCurrentExportedTasks(JSON.stringify(tasks));
+            <button type="button" onClick={() => {
+                setCurrentExportedTasks(JSON.stringify([...tasks]));
                 setExportModalVisible(true);
             }}>Export / Import</button>
             <Modal title={"Export / Import"} show={exportModalVisible} onHide={() => setExportModalVisible(false)}>
                 <form onSubmit={(e) => {
                     e.preventDefault();
-                    const updatedTasks = JSON.parse(e.target.importValue.value);
-                    let sameConfirm = !(e.target.importValue.value !== null && currentExportedTasks !== e.target.importValue.value);
-                    if (sameConfirm && importMerge) {
-                        sameConfirm = confirm('No changes are detected to import. By merging, you are about to create duplicates. Are you sure you want to merge ? ');
-                    }
-                    if (importMerge && sameConfirm) {
-                        updateTasks([...tasks, ...updatedTasks]);
+                    let updatedTasks = JSON.parse(currentExportedTasks);
+                    let currentTasks = JSON.stringify([...tasks]);
+                    let sameAsLocal = currentExportedTasks === currentTasks;
+                    let differentFromLocal = currentExportedTasks !== currentTasks;
+                    if (importMerge) {
+                        let sameConfirm = true;
+                        if (sameAsLocal && importMerge) {
+                            sameConfirm = confirm('No changes are detected to import. By merging, you are about to create duplicates. Are you sure you want to merge ? ');
+                        }
+                        if (sameConfirm) {
+                            updateTasks([...tasks, ...updatedTasks]);
+                            setExportModalVisible(false);
+                        }
                     }
                     else if (!importMerge) {
-                        updateTasks(updatedTasks);
+                        let eraseConfirm = sameAsLocal;
+                        if (!importMerge && differentFromLocal) {
+                            eraseConfirm = confirm('Are you sure you want to import these values ? ');
+                        }
+                        if (eraseConfirm) {
+                            updateTasks([...updatedTasks]);
+                            setExportModalVisible(false);
+                        }
                     }
-                    setExportModalVisible(false);
                     return false;
                 }}>
                     <p>
@@ -36,10 +47,10 @@ export default function TaskExporter({tasks, updateTasks}) {
                                     cols={50}
                                     class="w-10/12"
                                     name="importValue"
-                                    onClick={(e) => {
-                                        e.target.setSelectionRange(0, e.target.value.length)
-                                    }}
-                                >{currentExportedTasks}</textarea>
+                                    id="importValue"
+                                    value={currentExportedTasks}
+                                    onInput={(e) => setCurrentExportedTasks(e.target.value)}
+                                ></textarea>
                     </p>
                     <p>
                         <button type="button" onClick={() => navigator.clipboard.writeText(currentExportedTasks)}>Copy to clipboard</button>
